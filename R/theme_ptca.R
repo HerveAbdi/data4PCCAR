c589f1e56b7f2d80ea033666674962a431af8b91
Herve Abdi <herve@utdallas.edu>
2019-09-10 21:17:41 -0500

    from theme_ptca to theme_ptca2

--- R/theme_ptca.R
+++ R/theme_ptca.R
@@ -13,9 +13,13 @@
 #' @title  A \code{ggplot2} theme that matches the defaults
 #' of the factorial figures in the package \code{PTCA4CATA}
 #'
-#' @description \code{theme_ptca}:
+#' @description \code{theme_ptca2}:
 #' A \code{ggplot2} theme that matches the defaults
 #' of the factorial figures in \code{PTCA4CATA}.
+#' \code{theme_ptca2} is obviously a (slightly smarter) 
+#' cousin of 
+#' \code{PTCA4CATA::theme_ptca} (which will be deprecated in the
+#' new future).
 #'
 #' @param mire (Default = \code{TRUE}) when \code{TRUE}
 #' plot the factorial axis at values \code{x0} and \code{y0}.
@@ -29,9 +33,9 @@
 #' the units are set by \code{ggplot2}, if a number is provided
 #' it will give the ratio (i.e., Y/X) to be used.
 #' @param base_family the font family for the text 
-#' (e.g., on a mac 'Times', 'Arial', 'Courier', 'Symbol' ,
-#' or plain R, 'sans', 'serif', 'mono', can be other fonts 
-#' if installed). Default is "" (i.e., uses the current default)
+#' (e.g., on a mac \code{'Times', 'Arial', 'Courier', 'Symbol'},
+#' or plain \code{R, 'sans', 'serif', 'mono'}, can also be other fonts 
+#' if installed). Default is "" (i.e., uses the current default).
 #' @author Vincent Guillemot & Hervé Abdi
 #' @import ggplot2
 #' @importFrom grDevices adjustcolor
@@ -39,13 +43,14 @@
 #'\dontrun{
 #' x <- c(5, 8, 1, 70)
 #' y <- c(10, 12, 20, 50)
-#' (p <-  ggplot(as.data.frame(cbind(x, y)),
-#'               aes(x = x, y = y)) +
-#'     geom_point(size = 2, shape = 19) +
-#'     theme_ptca())
+#' ( # parentheses are for print
+#'  p <-  ggplot(as.data.frame(cbind(x, y)), aes(x = x, y = y)) +
+#'            geom_point(size = 2, shape = 19) +
+#'            theme_ptca2() 
+#'            )
 #' }
 #' @export
-theme_ptca <- function(mire = TRUE, x0 = 0, y0 = 0, 
+theme_ptca2 <- function(mire = TRUE, x0 = 0, y0 = 0, 
                        coor.fixed = TRUE, 
                        base_family = "") {
   dark.col  <- '#26004d'  # a darker version of purple
@@ -79,7 +84,7 @@ theme_ptca <- function(mire = TRUE, x0 = 0, y0 = 0,
     theme_list <- append(theme_list, coord_fixed(coor.fixed))
       # if 1: will force the graph to be a square
   }
-  return( theme_list  )
+  return(theme_list)
 }
 # end of theme_ptca() ----
 # ____________________________________________________________________
